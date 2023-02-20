f20ba935864125c2da08a937786bc241a3dbde5e
Charlotte <charlottecasner@gmail.com>
2023-02-20 14:20:57 -0500

    Adds solution for different delimiter csv upload - saves csv file locally to be opened and read by csv sniffer to get delimiter then uses pandas to read the csv

--- app/views.py
+++ app/views.py
@@ -1,11 +1,6 @@
-import base64
-import io
-from flask import Flask, render_template, request, Response
+import csv
+from flask import render_template, request
 import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
-import numpy as np
 
 from app.show_graph import CreateGraph
 from . import app
@@ -23,18 +18,25 @@ def example():
 def upload():
     if request.method == 'POST':
         column = request.form['text']
+        
         try:
-            data = pd.read_csv(request.files.get('file'), usecols=[column], sep=',', engine='python')
+            uploaded_file = request.files.get('file')
+            if uploaded_file.filename != '':
+                uploaded_file.save(uploaded_file.filename)
+        
+            with open(uploaded_file.filename, newline='') as csvfile:
+                dialect = csv.Sniffer().sniff(csvfile.readline())
+                csvfile.seek(0)
+                data = pd.read_csv(uploaded_file.filename, usecols=[column], sep=dialect.delimiter, engine='python')
+                CreateGraph(data, column).create_graph_image()
 
-            image = CreateGraph(data, column).create_graph_image()
 
         except ValueError:
             message = 'Column not found, please try again'
             return render_template('upload_error.html', message=message)
+
         return render_template('upload.html', tables=[data.to_html()], titles=[''])
     return render_template('upload.html')
-
-
 
 
 @app.route('/show_data', methods=['GET', 'POST'])
@@ -43,128 +45,9 @@ def show_data():
 
         image = '/static/images/plot.png'
 
-        # return render_template('show_data.html', url=image)
-
-    #     list_of_firsts = []
-    #     for num in data[col_to_sort].values:
-    #         # convert num to str to get first digit, return as int
-    #         first_num = int(str(num)[0])
-    #         # add digits to list of all firsts
-    #         list_of_firsts.append(first_num)
-
-    #     # convert list to pandas series
-    #     sr = pd.Series(list_of_firsts)
-    #     # Print the series
-    #     print(sr)
-    #     # Print the value counts for each digit in the series
-    #     print(sr.value_counts())
-    #     sr_counts = sr.value_counts()
-    #     data = sr_counts.to_frame()
-    #     # create_graph()
-
         return render_template('show_data.html', url=image)
-    # return render_template('show_data.html')
+    return render_template('show_data.html', url=image)
 
-
-
-# Returns pandas series containing actual counts for each leading digit
-def get_actual_counts():
-
-    col_to_sort = '7_2009'
-
-    df = pd.read_csv('sample_data.csv', sep = '\t', usecols=[col_to_sort])
-
-    list_of_firsts = []
-    for num in df[col_to_sort].values:
-        # convert num to str to get first digit, return as int
-        first_num = int(str(num)[0])
-        # add digits to list of all firsts
-        list_of_firsts.append(first_num)
-
-    # convert list to pandas series
-    sr = pd.Series(list_of_firsts)
-    # Print the series
-    print(sr)
-    # Print the value counts for each digit in the series
-    print(sr.value_counts())
-    sr_counts = sr.value_counts()[:9]
-    # data = sr_counts.to_frame()
-
-    data = sr.value_counts()[:9]
-
-    return data
-
-
-# Returns array of expected counts based on Benford's law 
-def get_expected_counts():
-
-    col_to_sort = '7_2009'
-
-    df = pd.read_csv('sample_data.csv', sep = '\t', usecols=[col_to_sort])
-    print(df.size)
-
-    benford_percentages = [.301, .176, .125, .097, .079, .067, .058, .051, .046]
-    print('BEFORE: ', benford_percentages)
-    benford_results = [(i * df.size).round() for i in benford_percentages]
-    print('AFTER: ', benford_results)
-
-    return benford_results
-
-
-# Create bar graph comparing results 
-# benford = get_expected_counts()
-# actual = get_actual_counts().to_numpy()
-
-# plotdata = pd.DataFrame({
-
-#     "Benford": benford,
-
-#     "Actual": actual})
-
-# plotdata.plot(
-#     kind="bar",
-#     figsize=(15, 8),
-#     title = "Benford's Law vs. Actual Counts",
-#     xlabel = "Leading Digit",
-#     ylabel = "Total Count"
-#     )
-
-
-# plt.plot(data=plotdata)
-# plt.savefig('app/static/images/new_plot.png')
-# # plt.show()
-
-
-benford = get_expected_counts()
-actual = get_actual_counts().to_numpy()
-
-digits = (1,2,3,4,5,6,7,8,9)
-plotdata = {
-    "Benford": benford,
-    "Actual": actual
-}
-
-x = np.arange(len(benford))  # the label locations
-width = 0.3  # the width of the bars
-multiplier = 0
-
-fig, ax = plt.subplots(figsize=(15, 8))
-
-for attribute, measurement in plotdata.items():
-    offset = width * multiplier
-    rects = ax.bar(x + offset, measurement, width, label=attribute)
-    ax.bar_label(rects, padding=3)
-    multiplier += 1
-
-ax.set_xlabel("Leading Digit")
-ax.set_ylabel("Total Count")
-ax.set_title("Benford's Law vs. Actual Counts")
-ax.set_xticks(x + 0.15, digits)
-ax.legend(loc='upper right', ncols=3)
-
-fig.savefig('app/static/images/new_plot.png', dpi=200)
-
-# plt.show()
 
 
 if __name__ == "__main__":
